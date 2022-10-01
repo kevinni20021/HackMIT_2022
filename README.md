7bcc5108484e3edaa2d521db03be4cf809c92628
lucaschew <lucaschew0@gmail.com>
2022-10-01 13:49:55 -0400

    Initial Grayscale opencv

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # HackMIT_2022
+
+TBD
+
+Requirements to run:
+
+- opencv-python
+
+By:
+Chitra Mukherjee
+Khushi Adukia
+Kevin Ni
+Lucas Chew
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+import cv2 as cv
+
+vid = cv.VideoCapture(0)
+
+while True:
+    ret, frame = vid.read()
+
+    grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame', frame)
+    cv.imshow('gray', grayscale)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.relase()
+
+cv.destroyAllWindows()
