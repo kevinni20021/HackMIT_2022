ade295497201b7060c4334f097e1abd195106b3c
Kevin Ni <kevin.kni.ni@gmail.com>
2022-10-01 18:25:52 -0400

    Added initial neural network

--- CNN.py
+++ CNN.py
@@ -2,9 +2,9 @@ import numpy as np
 import os
 import tensorflow as tf
 
-batch_size=32
-img_height=64
-img_width=64
+BATCH_SIZE = 32
+IMG_HEIGHT = 64
+IMG_WIDTH = 64
 
 base_dir = os.path.join(os.getcwd(), "./ASL_dataset/")
 train_dir = os.path.join(base_dir, 'asl_alphabet_train/asl_alphabet_train')
@@ -13,5 +13,49 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     validation_split=0.1,
     subset="training",
     seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE)
+
+validation_ds = tf.keras.utils.image_dataset_from_directory(
+    train_dir,
+    validation_split=0.09,
+    subset="validation",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    train_dir,
+    validation_split=0.01,
+    subset="validation",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE)
+
+class_names = train_ds.class_names
+#creating the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1./255),
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.25),
+
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.25),
+
+    tf.keras.layers.Conv2D(128, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.25),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(len(class_names), activation='softmax')
+
+])
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+history = model.fit(train_ds, batch_size=32,validation_batch_size=32, validation_data=validation_ds,epochs=10)
