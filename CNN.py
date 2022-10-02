0c0f46e40e7ef7cf96805ba44802e1bab3c4ef0d
Kevin Ni <kevin.kni.ni@gmail.com>
2022-10-01 20:36:57 -0400

    MORE epochs

--- CNN.py
+++ CNN.py
@@ -58,5 +58,5 @@ model = tf.keras.Sequential([
 ])
 
 model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-history = model.fit(train_ds, batch_size=32,validation_batch_size=32, validation_data=validation_ds,epochs= 3)
+history = model.fit(train_ds, batch_size=32,validation_batch_size=32, validation_data=validation_ds,epochs= 10)
 model.save("ASL_CNN.model")
